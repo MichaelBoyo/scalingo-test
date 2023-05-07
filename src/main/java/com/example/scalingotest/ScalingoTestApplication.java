db74e188428c3368417bd3199ee6f0e42d9e257d
Michael Boyo <boyomichaelbidemi@gmail.com>
2023-05-07 22:51:18 +0100

    kj

--- src/main/java/com/example/scalingotest/ScalingoTestApplication.java
+++ src/main/java/com/example/scalingotest/ScalingoTestApplication.java
@@ -2,12 +2,21 @@ package com.example.scalingotest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ScalingoTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScalingoTestApplication.class, args);
+    }
+    @CrossOrigin("*")
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
     }
 
 }
